17ea798781386c706feb2139331d89d0c33357ea
Steven Macenski <stevenmacenski@gmail.com>
2017-01-23 20:10:52 -0600

    adding plots

--- hw1/prob_pyramids/plot_ffts.m
+++ hw1/prob_pyramids/plot_ffts.m
@@ -4,13 +4,13 @@ figure(2)
 ha2 = tight_subplot(rows,cols,[.01 .03],[.1 .01],[.01 .01]);
            for ii = 1:9;
                if ii < 6;
-                 colormap gray;
                  axes(ha2(ii));
                  imagesc(log(abs(fftshift(fft2(gaussian{ii})))));
-               elseif ii < 10
                  colormap gray;
+               elseif ii < 10
                  axes(ha2(ii));
                  imagesc(log(abs(fftshift(fft2(laplacian{ii-cols})))));
+                 colormap gray;
                end
            end
 end
