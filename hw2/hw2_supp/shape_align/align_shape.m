a447f6c6c1a7f626938208e24f9771417b8655ad
Steven Macenski <stevenmacenski@gmail.com>
2017-02-20 22:24:36 -0600

    continued work

--- hw2/hw2_supp/shape_align/align_shape.m
+++ hw2/hw2_supp/shape_align/align_shape.m
@@ -1,10 +1,10 @@
-function T = align_shape( im1, im2 )
+function [T,im_aligned] = align_shape( im1, im2 )
 % align_shape to align images
 %   Steve Macenski (c) 2017
 
 tic;
 
-%find mean locations and translation
+%find mean locations
 [xIm1,yIm1] = find(im1);
 [xIm2,yIm2] = find(im2);
 meanX1 = mean(xIm1);
@@ -12,42 +12,72 @@ meanX2 = mean(xIm2);
 meanY1 = mean(yIm1);
 meanY2 = mean(yIm2);
 
-translation = [meanX2-meanX1; meanY2-meanY1];
-
-% find diff in scale
+% find diff in scale setup
 dist1 = hypot(xIm1-meanX1,yIm1-meanY1);
 dist2 = hypot(xIm2-meanX2,yIm2-meanY2);
 dist1 = sum(dist1)/length(xIm1);
 dist2 = sum(dist2)/length(xIm2);
 
+% rotation by variance of scaled object shape setup
+rot1=pca([xIm1, yIm1]);
+rot2=pca([xIm2, yIm2]);
+
+%rotation, scale, and translation
+rot=rot2/rot1;
 scale = dist2/dist1;
+translation = scale*[meanX2-meanX1; meanY2-meanY1]; %scale here, TODO
+%overshooting the translation every time
 
-% find rotation by variance of scaled object shape
-x1 = (xIm1-meanX1)/length(xIm1)*scale;
-y1 = (yIm1-meanY1)/length(yIm1)*scale;
-x2 = (xIm1-meanX1)/length(xIm2);
-y2 = (yIm1-meanY1)/length(yIm2);
-rot1 = atan2(y1,x1);
-rot2 = atan2(y2,x2);
+% initial transformation
+T = eye(4);
+translation = scale*rot*translation;
+T = [rot*scale, [translation(1);translation(2)]];
 
-rot = rot2-rot1;
+im_aligned = zeros(size(im1));
+im1pts = [xIm1, yIm1];
+im1pts = [im1pts ones(size(xIm1))];
+im2pts = [xIm2, yIm2];
 
-% scale image 1, transform, and rotate from initial findings 
+for i = 1:7
+    
+    im1to2pts = transpose(T*im1pts');
+    
+    for j = 1:size(im1pts,1)
+       
+        if (round(im1to2pts(j,1))>=0 &&... 
+            round(im1to2pts(j,2))>=0 &&...
+            round(im1to2pts(j,1))<=size(im1,1) &&...
+            round(im1to2pts(j,2))<=size(im1,2))
+        
+              im_aligned(round(im1to2pts(j,1)),...
+                         round(im1to2pts(j,2))) = 1; 
+        end
+    end
+  
+    error(i) = evalAlignment(im_aligned, im2);
+    
+     %find k neighbors
+     [IDX, D] = knnsearch(im2pts, im1to2pts);
+%     
+%     tx = im2pts(IDX,:);
+%     A = zeros(size(im1pts)*2);
+%     for k = 1:size(im1pts,1)
+%        A(2*k-1,1:3) = im1pts(k,:);
+%        A(2*k,4:6) = im1pts(k,:);
+%     end
+%     
+%     %find new transformation
+%     tx = tx';
+%     b = tx(:);
+%     T  =A\b;
+%     T = reshape(T,[3,2])';
+    
+end
 
+time_to_run = toc; %unsuppress to display
 
-%iterate
-%       TODO assign each point in 1 to nearest neighbor in 2
-
-%       TODO estimate transformation params
-
-
-im_align = [];
-
-
-time_to_run = tock %unsuppress to display
-
-figure (1)
-imshow(displayAlignment(im1,im2,im_align));
+% figure (4)
+% imshow(im_aligned)
 
 end
 
--- hw2/hw2_supp/shape_align/runThis.m
+++ hw2/hw2_supp/shape_align/runThis.m
@@ -1,11 +1,22 @@
 % Testing HW2 P2
 %Steve Macenski (c) 2017
 
+im1 = imread('object1.png')>0;
+im2 = imread('object2.png')>0;
+im2t = imread('object2t.png')>0;
+im3 = imread('object3.png')>0;
+
 % image 1 to 2
-Ta = align_shape(imread('object2.png')>0,imread('object1.png')>0);
+[Ta,im_alignedA] = align_shape(im1,im2);
+figure(1)
+imshow(displayAlignment(im1,im2,im_alignedA));
 
 % image 2 to 2t
-Tb = align_shape(imread('object2.png')>0,imread('object2t.png')>0);
+[Tb,im_alignedB] = align_shape(im2,im2t);
+figure(2)
+imshow(displayAlignment(im2,im2t,im_alignedB));
 
 %image 2 to 3
-Tc = align_shape(imread('object2.png')>0,imread('object3.png')>0);
+[Tc,im_alignedC] = align_shape(im2,im3);
+figure(3)
+imshow(displayAlignment(im2,im3,im_alignedC));
