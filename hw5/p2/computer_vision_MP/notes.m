6a0b761827003f545290cf4682772d0b0f8ed966
Macenski <smacens2@illinois.edu>
2017-04-20 18:13:22 -0500

    network deeeeeeper

---
+++ hw5/p2/computer_vision_MP/notes.m
@@ -0,0 +1,18 @@
+https://github.com/mfigurnov/perforated-cnn-matconvnet/blob/master/examples/cnn_cifar.m
+
+https://github.com/vlfeat/matconvnet/blob/master/examples/cifar/cnn_cifar_init.m
+
+https://github.com/vlfeat/matconvnet/blob/master/examples/cifar/cnn_cifar_init_nin.m
+
+https://courses.engr.illinois.edu/cs543/sp2017/lectures/Lecture%2022%20-%20CNNs%20-%20%20Vision_Spring2017.pdf
+slide 43
+
+http://www.vlfeat.org/matconvnet/functions/
+
+https://adeshpande3.github.io/adeshpande3.github.io/A-Beginner's-Guide-To-Understanding-Convolutional-Neural-Networks/
+
+https://adeshpande3.github.io/adeshpande3.github.io/A-Beginner's-Guide-To-Understanding-Convolutional-Neural-Networks-Part-2/
+
+http://cs231n.github.io/convolutional-networks/
+
+https://inclass.kaggle.com/c/sp17-cs543-uiuc
